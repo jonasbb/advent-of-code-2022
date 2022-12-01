3efcd867a6b351ed32211f19fadcafe261415985
Jonas Bushart <jonas@bushart.org>
2022-12-01 21:28:33 +0100

    Cleanup

--- src/prelude.rs
+++ src/prelude.rs
@@ -4,7 +4,6 @@ pub type Result<T, E = anyhow::Error> = std::result::Result<T, E>;
 pub use anyhow::anyhow;
 pub use itertools::Itertools as _;
 pub use rayon::prelude::*;
-pub use recap::{Recap, Regex};
 pub use serde::{Deserialize, Serialize};
 pub use std::collections::{
     BTreeMap, BTreeMap as Map, BTreeSet, BTreeSet as Set, HashMap, HashSet,
